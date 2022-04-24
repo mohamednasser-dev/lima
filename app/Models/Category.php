8002cffcbe27c6b17df7398df04563d914c1cf54
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-04-24 18:20:35 +0200

    fix categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function getImageAttribute($image)
+    {
+        if (!empty($image)) {
+            return asset('uploads/categories') . '/' . $image;
+        }
+        return asset('default-image.png');
+    }
+
+    public function setImageAttribute($image)
+    {
+        if (is_file($image)) {
+            $img_name = time() . uniqid() . '.' . $image->getClientOriginalExtension();
+            $image->move(public_path('/uploads/Slider/'), $img_name);
+            $this->attributes['image'] = $img_name;
+        } else {
+            $this->attributes['image'] = $image;
+        }
+    }
 }
