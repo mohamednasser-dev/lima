09af3d81a4af1542112546920e22dedced65ec92
MostafaElebzary <mostafaelebzary@gmail.com>
2022-04-26 05:44:04 +0200

    m

--- resources/views/frontend/layout/head.blade.php
+++ resources/views/frontend/layout/head.blade.php
@@ -10,7 +10,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>{{\App\Models\Setting::where('key', 'site_name_'.session('lang'))->first()->val}} - @yield('title')</title>
     <link rel="alternate" type="application/rss+xml"
-          title="{{\App\Models\Setting::where('key', 'site_name_'.session('lang'))->first()->val}}  &raquo; Comments Feed" href="#"/>
+          title="{{\App\Models\Setting::where('key', 'site_name_'.session('lang'))->first()->val}}  &raquo; Comments Feed"
+          href="#"/>
     <link rel="shortcut icon" href="{{url('/')}}/{{\App\Models\Setting::where('key', 'logo')->first()->val}}">
     <link rel="stylesheet" href="{{url('front')}}/css/font-awesome.css">
     @if(session('lang') == "ar")
@@ -27,6 +28,20 @@
     @else
         <link rel="stylesheet" type="text/css" href="{{url('front')}}/revslider/styles.css"/>
     @endif
+    <style>
+        body {
+
+            -webkit-touch-callout: none; /* iOS Safari */
+            -webkit-user-select: none; /* Safari */
+            -khtml-user-select: none; /* Konqueror HTML */
+            -moz-user-select: none; /* Old versions of Firefox */
+            -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none;
+            /* Non-prefixed version, currently
+                                             supported by Chrome, Edge, Opera and Firefox */
+        }
+
+    </style>
 </head>
 
 
@@ -55,21 +70,23 @@
                 <li class='lang_bar'>
                     <div id="lang_sel">
                         @if(session('lang') == "ar")
-                        <ul>
-                            <li>
-                                <a href="#" class="lang_sel_sel icl-en"><img class="iclflag"
-                                                                             src="{{url('front')}}/images/ar.png"
-                                                                             alt="ar" title="العربية"/> &nbsp;
-                                </a>
-                                <ul>
-                                    <li class="icl-fr">
-                                        <a href="{{url('lang/en')}}"><img class="iclflag" src="{{url('front')}}/images/en.png" alt="en"
-                                                         title="English"/>&nbsp;</a>
-                                    </li>
+                            <ul>
+                                <li>
+                                    <a href="#" class="lang_sel_sel icl-en"><img class="iclflag"
+                                                                                 src="{{url('front')}}/images/ar.png"
+                                                                                 alt="ar" title="العربية"/> &nbsp;
+                                    </a>
+                                    <ul>
+                                        <li class="icl-fr">
+                                            <a href="{{url('lang/en')}}"><img class="iclflag"
+                                                                              src="{{url('front')}}/images/en.png"
+                                                                              alt="en"
+                                                                              title="English"/>&nbsp;</a>
+                                        </li>
 
-                                </ul>
-                            </li>
-                        </ul>
+                                    </ul>
+                                </li>
+                            </ul>
                         @else
                             <ul>
                                 <li>
@@ -79,8 +96,10 @@
                                     </a>
                                     <ul>
                                         <li class="icl-fr">
-                                            <a href="{{url('lang/ar')}}"><img class="iclflag" src="{{url('front')}}/images/ar.png" alt="ar"
-                                                             title="العربية"/>&nbsp;</a>
+                                            <a href="{{url('lang/ar')}}"><img class="iclflag"
+                                                                              src="{{url('front')}}/images/ar.png"
+                                                                              alt="ar"
+                                                                              title="العربية"/>&nbsp;</a>
                                         </li>
 
                                     </ul>
@@ -89,14 +108,18 @@
                         @endif
                     </div>
                 </li>
-                <li><a href="{{\App\Models\Setting::where('key', 'google')->first()->val}}" title="Google Plus" target="_blank"><i
+                <li><a href="{{\App\Models\Setting::where('key', 'google')->first()->val}}" title="Google Plus"
+                       target="_blank"><i
                             class="fa fa-google-plus fa-2x"></i></a><span style="background-color:#dd4b39;"></span></li>
-                <li><a href="{{\App\Models\Setting::where('key', 'facebook')->first()->val}}" title="Facebook" target="_blank"><i
+                <li><a href="{{\App\Models\Setting::where('key', 'facebook')->first()->val}}" title="Facebook"
+                       target="_blank"><i
                             class="fa fa-facebook fa-2x"></i></a><span style="background-color:#3b5998;"></span></li>
-                <li><a href="{{\App\Models\Setting::where('key', 'youtube')->first()->val}}" title="Youtube" target="_blank"><i
+                <li><a href="{{\App\Models\Setting::where('key', 'youtube')->first()->val}}" title="Youtube"
+                       target="_blank"><i
                             class="fa fa-youtube-play fa-2x"></i></a><span style="background-color:#b31217;"></span>
                 </li>
-                <li><a href="{{\App\Models\Setting::where('key','twitter')->first()->val}}" title="Twitter" target="_blank"><i
+                <li><a href="{{\App\Models\Setting::where('key','twitter')->first()->val}}" title="Twitter"
+                       target="_blank"><i
                             class="fa fa-twitter fa-2x"></i></a><span style="background-color:#4099ff;"></span></li>
 
 
@@ -104,7 +127,8 @@
             <!-- .kids_social -->
             <div class="kids_clear"></div>
             <div id="kids_logo_block" class="logo-position-left">
-                <a id="kids_logo_text" href="{{url('/')}}"><img style="max-height: 40%;max-width: 40%" src="{{url('/')}}/{{\App\Models\Setting::where('key', 'logo')->first()->val}}"
+                <a id="kids_logo_text" href="{{url('/')}}"><img style="max-height: 40%;max-width: 40%"
+                                                                src="{{url('/')}}/{{\App\Models\Setting::where('key', 'logo')->first()->val}}"
                                                                 alt="{{\App\Models\Setting::where('key', 'site_name_'.session('lang'))->first()->val}}"
                                                                 title="{{\App\Models\Setting::where('key', 'site_name_'.session('lang'))->first()->val}}"/></a>
             </div>
