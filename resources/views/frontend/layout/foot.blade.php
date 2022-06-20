8a128104905d4ed5bdb48cd7dcd6280e8e007141
MostafaElebzary <mostafaelebzary@gmail.com>
2022-06-20 23:29:14 +0200

    m

--- resources/views/frontend/layout/foot.blade.php
+++ resources/views/frontend/layout/foot.blade.php
@@ -150,57 +150,57 @@
 <script type='text/javascript' src='{{url('front')}}/js/jquery.isotope.min.js'></script>
 <script type='text/javascript' src='{{url('front')}}/js/jquery.flexnav.min.js'></script>
 <script type='text/javascript' src='{{url('front')}}/js/jquery.prettyPhoto.js'></script>
-{{--<script>--}}
+<script>
 
-{{--    document.addEventListener('contextmenu', event => event.preventDefault());--}}
-{{--    /** TO DISABLE SCREEN CAPTURE **/--}}
-{{--    document.addEventListener('keyup', (e) => {--}}
-{{--        if (e.key == 'PrintScreen') {--}}
-{{--            navigator.clipboard.writeText('');--}}
-{{--            alert('Screenshots disabled!');--}}
-{{--        }--}}
-{{--    });--}}
+    document.addEventListener('contextmenu', event => event.preventDefault());
+    /** TO DISABLE SCREEN CAPTURE **/
+    document.addEventListener('keyup', (e) => {
+        if (e.key == 'PrintScreen') {
+            navigator.clipboard.writeText('');
+            alert('Screenshots disabled!');
+        }
+    });
 
-{{--    /** TO DISABLE PRINTS WHIT CTRL+P **/--}}
-{{--    document.addEventListener('keydown', (e) => {--}}
-{{--        if (e.ctrlKey && e.key == 'p') {--}}
-{{--            alert('This section is not allowed to print or export to PDF');--}}
-{{--            e.cancelBubble = true;--}}
-{{--            e.preventDefault();--}}
-{{--            e.stopImmediatePropagation();--}}
-{{--        }--}}
-{{--    });--}}
+    /** TO DISABLE PRINTS WHIT CTRL+P **/
+    document.addEventListener('keydown', (e) => {
+        if (e.ctrlKey && e.key == 'p') {
+            alert('This section is not allowed to print or export to PDF');
+            e.cancelBubble = true;
+            e.preventDefault();
+            e.stopImmediatePropagation();
+        }
+    });
 
-{{--    /** TO DISABLE PRINTS WHIT CTRL+U **/--}}
-{{--    document.addEventListener('keydown', (e) => {--}}
-{{--        if (e.ctrlKey && e.key == 'u') {--}}
-{{--            alert('This section is not allowed to see source code');--}}
-{{--            e.cancelBubble = true;--}}
-{{--            e.preventDefault();--}}
-{{--            e.stopImmediatePropagation();--}}
-{{--        }--}}
-{{--    });--}}
+    /** TO DISABLE PRINTS WHIT CTRL+U **/
+    document.addEventListener('keydown', (e) => {
+        if (e.ctrlKey && e.key == 'u') {
+            alert('This section is not allowed to see source code');
+            e.cancelBubble = true;
+            e.preventDefault();
+            e.stopImmediatePropagation();
+        }
+    });
 
-{{--    document.onkeypress = function (event) {--}}
-{{--        event = (event || window.event);--}}
-{{--        if (event.keyCode == 123) {--}}
-{{--            return false;--}}
-{{--        }--}}
-{{--    }--}}
-{{--    document.onmousedown = function (event) {--}}
-{{--        event = (event || window.event);--}}
-{{--        if (event.keyCode == 123) {--}}
-{{--            return false;--}}
-{{--        }--}}
-{{--    }--}}
-{{--    document.onkeydown = function (event) {--}}
-{{--        event = (event || window.event);--}}
-{{--        if (event.keyCode == 123) {--}}
-{{--            return false;--}}
-{{--        }--}}
-{{--    }--}}
+    document.onkeypress = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+            return false;
+        }
+    }
+    document.onmousedown = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+            return false;
+        }
+    }
+    document.onkeydown = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+            return false;
+        }
+    }
 
-{{--</script>--}}
+</script>
 @include('sweetalert::alert')
 @yield('js')
 </body>
