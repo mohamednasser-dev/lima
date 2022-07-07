1c2b24e97ee823765899c068613f841c0da13939
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-07-07 18:26:40 +0200

    fix

--- app/Http/Controllers/Api/SubscriptionController.php
+++ app/Http/Controllers/Api/SubscriptionController.php
@@ -98,8 +98,6 @@ class SubscriptionController extends GeneralController
         } else {
             return msgdata($request, failed(), trans('lang.should_choose_valid_course'), (object)[]);
         }
-        return msgdata($request, failed(), 'user_phone', apiUser()->phone);
-
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://app.fawaterk.com/api/v2/invoiceInitPay',
